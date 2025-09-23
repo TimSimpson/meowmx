aedaf0f94f8f26ab4986d4a8cdd99baecc521259
unknown <timsimpson4@gmail.com>
2025-09-23 01:57:13 -0500

    adds build options

--- src/demolib.py
+++ src/demolib.py
@@ -10,12 +10,3 @@ def create_meowmx() -> meowmx.Client:
         "postgresql+psycopg://eventsourcing:eventsourcing@localhost:5443/eventsourcing?sslmode=disable"
     )
     return meowmx.Client(engine=engine)
-
-
-class CatCreated(meowmx.Event):
-    cat_name: str
-
-
-class CatUpdated(meowmx.Event):
-    new_random_value: str
-    version: int
--- src/meowmx/__init__.py
+++ src/meowmx/__init__.py
@@ -1,17 +1,12 @@
 from .client import Client, ExpectedVersionFailure
-from event_sourcery.event_store import Event, StreamId
-from event_sourcery.event_store.exceptions import ConcurrentStreamWriteError
 from .common import Engine, NewEvent, RecordedEvent, Session, SessionMaker
 
 __all__ = [
     "Client",
-    "ConcurrentStreamWriteError",
     "Engine",
-    "Event",
     "ExpectedVersionFailure",
     "NewEvent",
     "RecordedEvent",
     "Session",
     "SessionMaker",
-    "StreamId",
 ]
